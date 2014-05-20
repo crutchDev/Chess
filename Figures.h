e861177756e689bf021e6514d06a90de794b30ab
Podze Alexandr <dasdeg@gmail.com>
2014-05-20 23:18:39 +0300

    added empty figure classes

--- Figures.h
+++ Figures.h
@@ -6,13 +6,11 @@
 class King : public Figure {
 public :
 	bool isAttacked() {
-		
 		// TODO
 		return false;
 	}
 
 	virtual bool canMove( ChessboardPos& pos ) {
-	
 		// TODO
 		return false;
 	}
@@ -21,13 +19,123 @@ public :
 			// TODO
 	}
 
+	virtual vector< ChessboardPos > getAllowedMove() {
+
+		// TODO
+		return vector< ChessboardPos >();
+	}
+};
+
+class Pawn : public Figure{
+	void Transform(){
+		//TODO
+	}
+
+	bool isAttacked() {
+		// TODO
+		return false;
+	}
+
+	virtual bool canMove(ChessboardPos& pos) {
+		// TODO
+		return false;
+	}
+
+	virtual void move(ChessboardPos& pos) {
+		// TODO
+	}
 
 	virtual vector< ChessboardPos > getAllowedMove() {
 
 		// TODO
 		return vector< ChessboardPos >();
 	}
-
-
 };
 
+class Rook : public Figure{
+	bool isAttacked() {
+		// TODO
+		return false;
+	}
+
+	virtual bool canMove(ChessboardPos& pos) {
+		// TODO
+		return false;
+	}
+
+	virtual void move(ChessboardPos& pos) {
+		// TODO
+	}
+
+	virtual vector< ChessboardPos > getAllowedMove() {
+
+		// TODO
+		return vector< ChessboardPos >();
+	}
+};
+
+class Queen : public Figure {
+	bool isAttacked() {
+		// TODO
+		return false;
+	}
+
+	virtual bool canMove(ChessboardPos& pos) {
+		// TODO
+		return false;
+	}
+
+	virtual void move(ChessboardPos& pos) {
+		// TODO
+	}
+
+	virtual vector< ChessboardPos > getAllowedMove() {
+
+		// TODO
+		return vector< ChessboardPos >();
+	}
+};
+
+class Knight : public Figure{
+	bool isAttacked() {
+		// TODO
+		return false;
+	}
+
+	virtual bool canMove(ChessboardPos& pos) {
+		// TODO
+		return false;
+	}
+
+	virtual void move(ChessboardPos& pos) {
+		// TODO
+	}
+
+	virtual vector< ChessboardPos > getAllowedMove() {
+
+		// TODO
+		return vector< ChessboardPos >();
+	}
+};
+
+class Bishop : public Figure{
+	bool isAttacked() {
+		// TODO
+		return false;
+	}
+
+	virtual bool canMove(ChessboardPos& pos) {
+		// TODO
+		return false;
+	}
+
+	virtual void move(ChessboardPos& pos) {
+		// TODO
+	}
+
+	virtual vector< ChessboardPos > getAllowedMove() {
+
+		// TODO
+		return vector< ChessboardPos >();
+	}
+};
