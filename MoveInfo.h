692545ec095629ef12a7cdec0295b1fdf3242853
Podze Alexandr <dasdeg@gmail.com>
2014-05-20 22:33:51 +0300

    MoveInfo proper interface

--- MoveInfo.h
+++ MoveInfo.h
@@ -6,13 +6,13 @@
 class MoveInfo {
 public:
 	MoveInfo();
-	//MoveInfo(Figure::ChessboardPos beg, Figure::ChessboardPos end, Figure* _killed);
+	MoveInfo(ChessboardPos beg, ChessboardPos end, Figure* _killed);
 	~MoveInfo();
-	//Figure::ChessboardPos startPos, endPos;
+	ChessboardPos startPos, endPos;
 	Figure* killed;
 	bool transformed; //pawn to queen
 
 	//subject to change
-	//Chessboard* snapshot;
+	Chessboard* snapshot;
 };
 
