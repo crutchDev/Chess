7b2a8f664e31336fae9b908fe0ab36ffee4b0b49
MelnykAndriy <kukoban@ukr.net>
2014-05-20 23:39:06 +0300

    Update GameManager.cpp

--- GameManager.cpp
+++ GameManager.cpp
@@ -5,3 +5,18 @@
 
 GameManager::GameManager(void) {
 }
+
+void GameManager::gameActivity() {
+    while ( true ) {
+      	if ( white->gameOver() ) {
+        		achivement(black);
+        		break;
+      	}
+      	white->step();
+      	if ( black->gameOver() ) {
+        		achivement(white);
+        		break;
+      	}				
+      	black->step();
+    }
+}
