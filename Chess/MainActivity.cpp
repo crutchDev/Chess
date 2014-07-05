dc0085de819a6158784c862c22fb39da58c455be
MelnykAndriy <kukoban@ukr.net>
2014-07-05 13:30:47 +0300

    Added sfml support.

--- Chess/MainActivity.cpp
+++ Chess/MainActivity.cpp
@@ -6,13 +6,41 @@
 *last modified: 17/05/2014
 *************************************************************************/
 
+
+#include <SFML/Graphics.hpp>
 #include "chessInclude.h"
+#include <thread>
+
+using namespace sf;
 
 int main ()  {
+	
+	thread window([] ()->void {
+		sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+		sf::CircleShape shape(100.f);
+		shape.setFillColor(sf::Color::Green);
+
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			window.clear();
+			window.draw(shape);
+			window.display();
+		}
+		
+	});
+
 	ConsoleInterface iface;
 	GameManager manager(&iface);
 	manager.gameActivity();
 
 
+		
 	return 0;
 }
